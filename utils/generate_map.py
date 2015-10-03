223fe29664c5875f261d70e47903099e67471933
Despair <lfp002@yandex.ru>
2015-10-03 17:23:08 +0500

    load map from server

---
+++ utils/generate_map.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 3 окт. 2015 г.
+
+@author: despair
+'''
+import json
+import random
+
+data={"map_layer1":[]}
+with open('/home/despair/evedev/phaser_test/assets/map_layer1','w') as of:
+    for x in range(0,40):
+        d=[]
+        for y in range(0,30):
+            d.append(random.randint(0,2))
+        data['map_layer1'].append(d)
+    json.dump(data,of)
